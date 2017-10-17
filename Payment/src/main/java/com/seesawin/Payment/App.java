0618d9341167fb82d80b9acbba1e412c0eb4e3f1
seesawin <seesawin@gmail.com>
2017-10-17 11:21:16 +0800

    2017/10/17 First edition

---
+++ Payment/src/main/java/com/seesawin/Payment/App.java
@@ -0,0 +1,13 @@
+package com.seesawin.Payment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
